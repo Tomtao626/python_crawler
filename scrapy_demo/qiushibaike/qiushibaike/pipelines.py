9e2f8ad798c9cc7518a79cdf29e230290d104cf8
Tomtao626 <751825253@qq.com>
2020-04-11 00:51:44 +0800

    'next_url'

--- scrapy_demo/qiushibaike/qiushibaike/pipelines.py
+++ scrapy_demo/qiushibaike/qiushibaike/pipelines.py
@@ -7,16 +7,52 @@
 
 import json
 
+# class QiushibaikePipeline(object):
+#     def __init__(self):
+#         self.fp = open("duanzi.json", 'w', encoding='utf-8')
+#
+#     def open_spider(self, spider):
+#         print("爬虫开始。。。")
+#
+#     def process_item(self, item, spider):
+#         item_json = json.dumps(dict(item),ensure_ascii=False)
+#         self.fp.write(item_json+'\n')
+#         return item
+#
+#     def close_spider(self, spider):
+#         self.fp.close()
+#         print("爬虫结束。。。")
+
+from scrapy.exporters import JsonItemExporter,JsonLinesItemExporter
+
+# class QiushibaikePipeline(object):
+#     def __init__(self):
+#         self.fp = open("duanzi.json", 'wb')
+#         self.exporter = JsonItemExporter(self.fp, ensure_ascii=False,encoding='utf-8')
+#         self.exporter.start_exporting()
+#
+#     def open_spider(self, spider):
+#         print("爬虫开始。。。")
+#
+#     def process_item(self, item, spider):
+#         self.exporter.export_item(item)
+#         return item
+#
+#     def close_spider(self, spider):
+#         self.exporter.finish_exporting()
+#         self.fp.close()
+#         print("爬虫结束。。。")
+
 class QiushibaikePipeline(object):
     def __init__(self):
-        self.fp = open("duanzi.json", 'w', encoding='utf-8')
+        self.fp = open("duanzi.json", 'wb')
+        self.exporter = JsonLinesItemExporter(self.fp, ensure_ascii=False,encoding='utf-8')
 
     def open_spider(self, spider):
         print("爬虫开始。。。")
 
     def process_item(self, item, spider):
-        item_json = json.dumps(dict(item),ensure_ascii=False)
-        self.fp.write(item_json+'\n')
+        self.exporter.export_item(item)
         return item
 
     def close_spider(self, spider):
--- scrapy_demo/qiushibaike/qiushibaike/spiders/qsbk.py
+++ scrapy_demo/qiushibaike/qiushibaike/spiders/qsbk.py
@@ -7,6 +7,7 @@ from scrapy.selector.unified import SelectorList
 class QsbkSpider(scrapy.Spider):
     name = 'qsbk'  # 名字必须唯一
     allowed_domains = ['qiushibaike.com']
+    base_domain = 'https://www.qiushibaike.com'
     start_urls = ['https://www.qiushibaike.com/text/page/1/']
 
     def parse(self, response):
@@ -24,3 +25,18 @@ class QsbkSpider(scrapy.Spider):
             # yield duanzi
             item = QiushibaikeItem(author=author,content=content)
             yield item
+        next_url = response.xpath("//ul[@class='pagination']/li[last()]/a/@href").get()
+        if not next_url:
+            return
+        else:
+            yield scrapy.Request(self.base_domain+next_url, callback=self.parse)
+        # items = list()
+        # divs = response.xpath('//div[@class="col1 old-style-col1"]/div')
+        # for div in divs:
+        #     # selector
+        #     author = div.xpath('.//h2/text()').get().strip()
+        #     content = div.xpath('.//div[@class="content"]//text()').getall()
+        #     content = ",".join(content).strip()
+        #     item = QiushibaikeItem(author=author, content=content)
+        #     items.append(item)
+        # return items
