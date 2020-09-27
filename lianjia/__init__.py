9d8441d1f073a9d03656976d384664ce0a683da8
tom_tao <751825253@qq.com>
2020-09-27 14:52:27 +0800

    lianjie_hz

---
+++ lianjia/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 _*-
+""" 
+@author:tom_tao626 
+@license: Apache Licence 
+@file: __init__.py.py 
+@time: 2020/09/27
+@contact: tom@007vin.com
+@site:  
+@software: PyCharm 
+
+# code is far away from bugs with the god animal protecting
+    I love animals. They taste delicious.
+              ┏┓      ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃      ☃      ┃
+            ┃  ┳┛  ┗┳  ┃
+            ┃      ┻      ┃
+            ┗━┓      ┏━┛
+                ┃      ┗━━━┓
+                ┃  神兽保佑    ┣┓
+                ┃　永无BUG！   ┏┛
+                ┗┓┓┏━┳┓┏┛
+                  ┃┫┫  ┃┫┫
+                  ┗┻┛  ┗┻┛ 
+"""
