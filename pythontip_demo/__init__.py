fe02125761623e8323c628fe0f3ab40632bee9b4
tomtao <751825253@qq.com>
2020-12-04 18:07:21 +0800

    fixsome

--- pythontip_demo/__init__.py
+++ pythontip_demo/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python  
-#-*- coding:utf-8 _*-  
+# -*- coding:utf-8 _*-
 """ 
 @author:tom_tao626 
 @license: Apache Licence 
--- pythontip_demo/abstract_factory.py
+++ pythontip_demo/abstract_factory.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python  
-#-*- coding:utf-8 _*-  
+# -*- coding:utf-8 _*-
 """ 
 @author:tom_tao626 
 @license: Apache Licence 
@@ -9,3 +9,10 @@
 @site: xxxx.suizhu.net
 @software: PyCharm 
 """
+
+import requests
+
+url = "http://www.pythontip.com/python-patterns/detail/abstract_factory"
+
+content = requests.get(url)
+print(content.text)
