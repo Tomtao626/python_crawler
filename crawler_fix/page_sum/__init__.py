879fc25c511a074b9579c4dc64a916b10aea7256
tomtao <751825253@qq.com>
2021-01-07 13:14:51 +0800

    fixsome

---
+++ crawler_fix/page_sum/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python  
+# -*- coding:utf-8 _*-
+""" 
+@author:tom_tao626 
+@license: Apache Licence 
+@file: __init__.py.py 
+@time: 2021/01/07
+@contact: tp320670258@gmail.com
+@site: xxxx.suizhu.net
+@software: PyCharm 
+"""
